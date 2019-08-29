5829f43718ab90f2ec129a5eecb7eac7f16a87df
Jishnu Viswanath <jishnu.viswanath@beteasy.com.au>
2019-08-29 11:36:47 +1000

    feat: [PACT] Step 3.3 - Creating Your First Pact Test for the Consumer Client

---
+++ YourSolution/Consumer/tests/ConsumerPactTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using PactNet.Mocks.MockHttpService;
+using PactNet.Mocks.MockHttpService.Models;
+using Consumer;
+using System.Collections.Generic;
+
+namespace tests
+{
+    public class ConsumerPactTests: IClassFixture<ConsumerPactClassFixture>
+    {
+        private IMockProviderService _mockProviderService;
+        private string _mockProviderServiceBaseUri;
+
+        public ConsumerPactTests(ConsumerPactClassFixture fixture)
+        {
+            _mockProviderService = fixture.MockProviderService;
+            _mockProviderService.ClearInteractions(); //NOTE: Clears any previously registered interactions before the test is run
+            _mockProviderServiceBaseUri = fixture.MockProviderServiceBaseUri;
+        }
+    }
+}

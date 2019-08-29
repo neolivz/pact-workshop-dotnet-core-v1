c137b02db41e099e0d746d88ba5d7370f8d51064
Jishnu Viswanath <jishnu.viswanath@beteasy.com.au>
2019-08-29 11:20:44 +1000

    feat: [PACT] Empty Consumer Pact Class Fixture

---
+++ YourSolution/Consumer/tests/ConsumerPactClassFixture.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    // This class is responsible for setting up a shared
+    // mock server for Pact used by all the tests.
+    // XUnit can use a Class Fixture for this.
+    // See: https://goo.gl/hSq4nv
+    public class ConsumerPactClassFixture
+    {
+    }
+}
